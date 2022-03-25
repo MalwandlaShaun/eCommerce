58948c9e407cfafb8d6fcd5d90e14f02d4b23aa0
Malwandla Hlungwani <hlungwanimalwandlashaun@gmail.com>
2022-03-25 19:35:29 +0200

    Updates

---
+++ precache-manifest.1208ddc619703154395e4e947527cb8f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d907a48afe05ab2eefbccd4e7f25266",
+    "url": "/eCommerce/index.html"
+  },
+  {
+    "revision": "46a307b6b37947bff37a",
+    "url": "/eCommerce/static/css/2.d872b5c7.chunk.css"
+  },
+  {
+    "revision": "7c266a41e064e6ca7f85",
+    "url": "/eCommerce/static/css/main.1fb11458.chunk.css"
+  },
+  {
+    "revision": "46a307b6b37947bff37a",
+    "url": "/eCommerce/static/js/2.bff39349.chunk.js"
+  },
+  {
+    "revision": "fc067e8abe54824a406678f539a112f4",
+    "url": "/eCommerce/static/js/2.bff39349.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c266a41e064e6ca7f85",
+    "url": "/eCommerce/static/js/main.59cba5c5.chunk.js"
+  },
+  {
+    "revision": "716c5635f45e7cdee465",
+    "url": "/eCommerce/static/js/runtime-main.1b91071d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/eCommerce/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/eCommerce/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/eCommerce/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/eCommerce/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/eCommerce/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
